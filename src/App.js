98b87254a4ad98639160b6f66839badde9181b8e
swtrm <swarshmait@gmail.com>
2024-11-18 21:49:35 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,190 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id="mainbg">
+      <img
+        className="coverimage"
+        src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/model/aventador/aventador-sv/slider/aventador-sv.jpg"
+        alt="Cover Image"
+        width="100%"
+        height="336px"
+      />
+      <div id="Namephoto">
+        <div>
+          <h1> Showroom SSCR </h1>
+        </div>
+        <div>
+          <img
+            src="https://cdn.leonardo.ai/users/c431464d-36c5-4329-847a-e4e80727329a/generations/28f7dd18-410a-4101-98c9-24abeef008d3/variations/Default_Ash_Ketchum_is_a_determined_and_adventurous_young_Pokm_0_28f7dd18-410a-4101-98c9-24abeef008d3_1.jpg"
+            alt="Display Picture"
+            width="300px"
+            height="300px"
+          />
+        </div>
+      </div>
+      <div id="pokemontrainer">
+        <div>
+          <p>
+          The car showroom is a sleek, modern space designed to showcase a variety of vehicles in a polished and professional environment. The large, glass-fronted facade allows natural light to flood the interior, highlighting the gleaming finishes of the cars on display. Inside, the showroom floor is spacious and meticulously clean, with a glossy tile or polished concrete surface that reflects the vehicles.
+          </p>
+        </div>
+      </div>
+      <div id="Expcontainer">
+        <div>
+          <h2>Fortuner</h2>
+          <h3>500 Juta</h3>
+          <p>
+          The Toyota Fortuner is a rugged and versatile SUV renowned for its powerful performance, robust build, and premium design. Designed to excel both on city roads and off-road adventures, the Fortuner combines luxury with utility, making it a popular choice among families, adventurers, and professionals alike.
+          </p>
+        </div>
+        <div>
+          <img className="image"
+            src="https://wallpaperaccess.com/full/1776035.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <img className="image"
+            src="https://carnetwork.s3.ap-southeast-1.amazonaws.com/file/b1ccfc35280a4098b3977412c57e7e75.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <h2>Innova</h2>
+          <h3>400 Juta</h3>
+          <p>
+            Successfully earned eight Gym Badges in
+            <br />
+            various regions, showcasing strategic thinking
+            <br />
+            and adaptability in battles against Gym
+            <br />
+            Leaders.
+          </p>
+        </div>
+        <div>
+          <h2>Civic Turbo Editon</h2>
+          <h3>1.4 Miliar</h3>
+          <p>
+            Consistently participated in regional Pokémon
+            <br />
+            League tournaments, reaching the finals in <br />
+            several instances and demonstrating a strong <br />
+            competitive spirit.
+          </p>
+        </div>
+        <div>
+          <img className="image"
+            src="https://i.ytimg.com/vi/pSMt0cZYHUA/maxresdefault.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <img className="image"
+            src="https://img.remediosdigitales.com/c2adeb/toyota-avanza-2022_/1366_2000.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <h2>Avanza</h2>
+          <h3>300 Juta</h3>
+          <p>
+            Known for forming deep connections with his
+            <br />
+            Pokémon partners, fostering strong friendships,
+            <br />
+            and valuing teamwork during battles.
+          </p>
+        </div>
+        <div>
+        <h2>Honda City</h2>
+        <h3>350 Juta</h3>
+          <p>
+            Conquered the Battle Frontier facilities in the <br />
+            Hoenn region, showcasing versatility in <br />
+            different battle formats.
+          </p>
+        </div>
+        <div>
+          <img className="image"
+            src="https://cdn.autopapo.com.br/box/uploads/2020/11/24142104/honda-city-hatchback-2021-vermelho-dianteira-detalhe.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <img className="image"
+            src="https://tconline.com.br/wp-content/uploads/2020/07/6.2-scaled.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <h2>Honda Civic</h2>
+          <h3>800 Juta</h3>
+          <p>
+            Played a crucial role in rescuing and caring <br />
+            for Pokémon in various instances, displaying a <br />
+            strong sense of responsibility and compassion.
+          </p>
+        </div>
+        <div>
+          <h2>Pajero</h2>
+          <h3>500 Juta</h3>
+          <p>
+            Exploring new regions and discovering rare <br />
+            and legendary Pokémon species.
+          </p>
+        </div>
+        <div>
+          <img className="image"
+            src="https://www.autoo.com.br/fotos/2023/10/1280_960/mitsubishi_pajero-sport_2024_1_30102023_76422_1280_960.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <img className="image"
+            src="https://imgcdn.oto.com/large/gallery/color/28/1635/mitsubishi-xpander-color-793751.jpg"
+            alt="Freepngimgcom"
+          />
+        </div>
+        <div>
+          <h2>Xpander</h2>
+          <h3>300 Juta</h3>
+          <p>
+            Inspiring and mentoring aspiring Pokémon
+            <br />
+            Trainers to reach their full potential.
+          </p>
+          <br />
+          <br />
+          <h3>Pokémon Researcher</h3>
+          <p>
+            Building a deeper understanding of Pokémon <br />
+            habitats and fostering conservation efforts.
+          </p>
+        </div>
+      </div>
+      <div id="education">
+        <div className="edu">
+          <h2>Education</h2>
+          <p>An educational car showroom is an innovative and interactive space designed to educate visitors about the automotive industry, technology, and sustainability. It combines the excitement of exploring vehicles with hands-on learning opportunities, making it ideal for students, enthusiasts, and curious minds of all ages.</p>
+          <h3>Visitor Experience</h3>
+          <p>The showroom provides a welcoming environment with knowledgeable staff guiding visitors. Informational brochures, interactive guides, and child-friendly activities ensure the experience is accessible and engaging for everyone.</p>
+        </div>
+        <div className="skills">
+          <h2>rating</h2>
+          <p>Experience ⭐⭐⭐⭐⭐</p>
+          <p>Quality ⭐⭐⭐⭐⭐</p>
+          <p>confortnes ⭐⭐⭐⭐⭐</p>
+          <h2>Contact</h2>
+          <p>(123) 456-7890</p>
+          <p>Pallet Town, Kanto Region</p>
+          <p>ash.ketchum@emailcom</p>
+          <p>www.ashketchum.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
